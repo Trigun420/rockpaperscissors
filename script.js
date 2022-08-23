123759e24d6111c3501104c9945559ec8851392d
Matt <Mattygb85@gmail.com>
2022-08-23 10:03:47 +0200

    second commit fixed loops

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let computerSelection;
 let resultMessage;
-let origin = (prompt("please enter rock, paper, or scissors").toLowerCase());
-let playerSelection = origin;
-
+let playerSelection;
 
 
 function getComputerChoice() {
@@ -19,13 +17,14 @@ function getComputerChoice() {
 }   
 //plays a single round and evaluates the results
 function playRound() {
-       
-        
+    let origin = (prompt("please enter rock, paper, or scissors").toLowerCase());
+    let playerSelection = origin;    
         computerSelection = getComputerChoice();
         console.log("Round Number " + gameCount)
         console.log(playerSelection)
+        
         if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")  {
-        return ("please enter a valid selection")  ;
+        return ("Please enter a valid selection.")  ;
     } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
         playerScore++; gameCount++;
         return ("You Win!! " + playerSelection + " beats " + computerSelection + ".");
@@ -46,15 +45,7 @@ function playRound() {
 }
     
 
-function result(){
-    
-    if ((gameCount === 5) && (playerScore > computerScore)) {
-    resultMessage = ("You win the game with a score of " + playerScore + " to " + computerScore);
-    } else if ((gameCount === 5) && (playerScore < computerScore)){
-    resultMessage =("You lost the war, fucko, with a score of " + playerScore + " to the computers score of " + computerScore)
-    } 
-        
-}
+
 
 
 
@@ -68,18 +59,28 @@ let playerScore = 0;
 function game() {
     
     
+   
     //plays 5 rounds
-    for (let i = 0; i < 5; i++) {
+    for (let i =1 ; i < 6; i++) {
         
       //logs scores to console
       console.log(playRound(computerSelection,playerSelection));
       console.log("Your score is " + playerScore);
       console.log("The computers score is " + computerScore);
-      
-      
+      console.log(result(gameCount, playerScore, computerScore))
+     
+        
     }
- }  
-
+}  
+function result(gameCount){
+    
+    if ((gameCount === 6) && (playerScore > computerScore)) {
+    return  ("You win the game with a score of " + playerScore + " to " + computerScore);
+    } else if ((gameCount === 6) && (playerScore < computerScore)){
+    return ("You lost the war, fucko, with a score of " + playerScore + " to the computers score of " + computerScore)
+    } 
+        
+}
 
 game();
 
